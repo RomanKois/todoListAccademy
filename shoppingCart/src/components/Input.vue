8a473ce7dd502e253828ce4dd8ed5fe91a230f0a
RomanKois <koisroman@gmail.com>
2023-01-05 16:07:52 +0100

    init

---
+++ shoppingCart/src/components/Input.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+  
+<script >
+export default {
+    name: "input",
+    data() {
+        return {
+            
+        };
+    }, 
+    methods: {
+       
+
+
+    }
+};
+</script>
+  
+<style scoped>
+
+</style>
